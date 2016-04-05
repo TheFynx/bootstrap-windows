5302cfad5094bf67519f9f87f84a1f31e32bfc42
Levi Smith <levi@fynx.me>
2016-04-04 22:51:19 -0500

    Cleaning up powershell script, bumping version

--- setup.ps1
+++ setup.ps1
@@ -4,26 +4,36 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
+# Install Chocolatey
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Setup no BOM encoded UTF8
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
 
+# Set Chef Home for this script
 $CHEFDIR='C:\chef\cookbooks'
 $env:HOME = $env:USERPROFILE
 
+# Install ChefDK
 C:\ProgramData\chocolatey\bin\choco.exe install -y chefdk
 
 # Add ChefDK to the path
 $env:Path += ";C:\opscode\chefdk\bin"
 
+# Write Out the Berksfile
 [System.IO.File]::WriteAllLines($CHEFDIR + 'Berksfile','source 'https://supermarket.chef.io'', $Utf8NoBomEncoding)
 [System.IO.File]::AppendAllText($CHEFDIR + 'Berksfile','cookbook "bootstrap_linux", path:' + '"' + $PSCommandPath + '"' + ([Environment]::NewLine), $Utf8NoBomEncoding)
 
 # Create client.rb
 [System.IO.File]::WriteAllLines($CHEFDIR + 'client.rb','cookbook_path File.join(Dir.pwd, 'berks-cookbooks')', $Utf8NoBomEncoding)
 
+# Switch to Chef Home to vendor cookbooks
 cd $CHEFDIR
 
 $env:BERKSHELF_CHEF_CONFIG = "$CHEFDIR\client.rb"
 
+# Vendor cookbook deps locally
 berks vendor
 
+# Run Chef!
 chef-client -z -c "$CHEFDIR\client.rb" -o bootstrap_windows
