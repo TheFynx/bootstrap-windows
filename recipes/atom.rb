5da14ba7fc87599fefaf3f8fbbf002529cdce311
Levi Smith <levi@fynx.me>
2016-04-05 21:05:00 -0500

    fixing atom recipe

--- recipes/atom.rb
+++ recipes/atom.rb
@@ -5,23 +5,23 @@
 # Copyright (C) 2016 Levi Smith
 #
 
-chocolatey atom
+chocolatey 'atom'
 
-if node['atom']['star_package_install']
-  execute 'apm star install' do
-    command "apm stars --user #{node['atom']['username']}"
-    action :run
-  end
+execute 'apm star install' do
+  command "apm stars --user #{node['atom']['username']}"
+  action :run
+  only_if { node['atom']['star_package_install'] }
 end
 
-if node['atom']['install_package_list']
-  template 'C:\tmp\packagelist.txt' do
-    source 'packagelist.txt.erb'
-    owner node['user']
-    mode 00774
-  end
-  execute 'apm package install' do
-    command 'apm install --packages-file C:\tmp\packagelist.txt'
-    action :run
-  end
+template 'C:\tmp\packagelist.txt' do
+  source 'packagelist.txt.erb'
+  owner node['user']
+  only_if { node['atom']['install_package_list'] }
+  mode 00774
+end
+
+execute 'apm package install' do
+  command 'apm install --packages-file C:\tmp\packagelist.txt'
+  action :run
+  only_if { node['atom']['install_package_list'] }
 end
